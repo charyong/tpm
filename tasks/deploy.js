6f7ea49f25f1543270d3f2b37b910881b7ecbdce
Longhao Luo <luolonghao@gmail.com>
2013-04-26 10:57:53 +0800

    Improve deploy task

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -5,16 +5,65 @@ var Rimraf = require('rimraf');
 var Iconv = require('iconv-lite');
 var ChildProcess = require('child_process');
 var Ftp = require('jsftp');
+var Ssh = require('ssh2');
 
 var Util = require(__dirname + '/../util');
 
-var DEPLOY_USAGE = 'Usage: tpm deploy [PATH] [SERVER]\n\n' +
+var DEPLOY_USAGE = 'Usage: tpm deploy [PATH] [ENV]\n\n' +
 	'Examples:\n' +
 	'tpm deploy dist/js/g.js beta\n' +
 	'tpm deploy project/TUILIB-65.txt beta\n';
 
+// Update static version
+// @param env: "beta", "wwwtest"
+function updateVersion(sshConfig, env) {
+	var ssh = new Ssh();
+
+	ssh.on('connect', function() {
+		Util.info('[SSH] connect ' + sshConfig.host);
+	});
+
+	ssh.on('ready', function() {
+		var cmd = '/usr/bin/python ' + sshConfig.root + '/updatever.py ' + env;
+
+		Util.info('[SSH] ' + cmd);
+
+		ssh.exec(cmd, function(err, stream) {
+			if (err) {
+				Util.error(err);
+				return;
+			}
+			stream.on('data', function(data, extended) {
+				if (extended === 'stderr') {
+					Util.error(data.toString());
+				} else {
+					Util.info(data.toString());
+				}
+			});
+			stream.on('exit', function(code, signal) {
+				ssh.end();
+			});
+		});
+	});
+
+	ssh.on('error', function(err) {
+		Util.info('[SSH] ' + err);
+	});
+
+	ssh.on('close', function(had_error) {
+		Util.info('[SSH] update version successful.');
+	});
+
+	ssh.connect({
+		host: sshConfig.host,
+		port: sshConfig.port,
+		username: sshConfig.user,
+		password: sshConfig.pass,
+	});
+}
+
 // Upload files by FTP
-function uploadByFtp(config, ftpConfig, paths) {
+function uploadByFtp(localRoot, ftpConfig, sshConfig, paths, env) {
 	var pathCount = paths.length;
 
 	Util.info('[FTP] connect ' + ftpConfig.host);
@@ -25,7 +74,7 @@ function uploadByFtp(config, ftpConfig, paths) {
 	for (var i = 0, len = paths.length; i < len; i++) {
 		var localPath = paths[i];
 
-		var relativePath = Path.relative(config.root, localPath).split(Path.sep).join('/');
+		var relativePath = Path.relative(localRoot, localPath).split(Path.sep).join('/');
 
 		var buffer = Fs.readFileSync(localPath);
 
@@ -46,6 +95,8 @@ function uploadByFtp(config, ftpConfig, paths) {
 						return;
 					}
 					Util.info('[FTP] upload successful.');
+
+					updateVersion(sshConfig, env);
 				});
 			}
 		});
@@ -62,14 +113,15 @@ exports.run = function(args, config) {
 	}
 
 	var path = args[0];
-	var testServer = args[1];
+	var env = args[1];
 
 	var pathList = [];
 
 	if (Util.indir(path, config.root + '/project')) {
 		pathList = Util.readProjectFile(config, path, 'dist');
 	} else {
-		if (!Util.indir(path, config.root + '/dist')) {
+		var stat = Fs.statSync(path);
+		if (!Util.indir(path, config.root + '/dist') || stat.isDirectory(path)) {
 			Util.error('Cannot deploy: ' + path);
 			return;
 		}
@@ -77,8 +129,8 @@ exports.run = function(args, config) {
 		pathList.push(path);
 	}
 
-	if (testServer == 'beta') {
-		uploadByFtp(config, config.ftp.beta, pathList);
+	if (env == 'beta') {
+		uploadByFtp(config.root, config.ftp.beta, config.ssh.beta, pathList, env);
 	}
 
 };
